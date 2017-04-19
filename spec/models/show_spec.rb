609661bf1946f075ee41c53a906da7393c21896f
amoralescr85 <amoralescr85@gmail.com>
2017-04-19 17:20:31 -0400

    Test change and react change

--- spec/models/show_spec.rb
+++ spec/models/show_spec.rb
@@ -3,8 +3,14 @@ require "rails_helper"
 feature "visitors can see a list of shows on homepage" do
   scenario "visitor sees shows displayed" do
 
-    game_of_thrones = Show.create(title: 'Game of Thrones', description: 'awesome HBO show', average_rating: 5)
-    californication = Show.create(title: 'Californication', description: 'awesome Showtime show', average_rating: 5)
+    game_of_thrones = Show.create(
+    title: 'Game of Thrones',
+    description: 'awesome HBO show',
+    average_rating: 5)
+    californication = Show.create(
+    title: 'Californication',
+    description: 'awesome Showtime show',
+    average_rating: 5)
 
     visit shows_path
 
