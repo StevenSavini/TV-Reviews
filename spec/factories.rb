420838afa3aee31988c479cbcf59289ebd0e0ecc
Harrison Muskat <harrisonmuskat@gmail.com>
2017-04-21 15:42:08 -0400

    Change tests

--- spec/factories.rb
+++ spec/factories.rb
@@ -5,14 +5,16 @@ FactoryGirl.define do
     first_name Faker::Name.first_name
     last_name Faker::Name.last_name
     username Faker::Internet.user_name
-    avatar_url Faker::Avatar.image
+    avatar Faker::Avatar.image
   end
 
   factory :show do
     sequence :title do |n|
       "Game of Thrones#{n}"
     end
-    description Faker::Hipster.sentence(3)
+    description "This is a long description that is supposed to talk about how
+                 great this show is and it's here mostly to take up space and
+                 make our page look good."
     average_rating 5
   end
 end
--- spec/features/06_user_views_all_shows_spec.rb
+++ spec/features/06_user_views_all_shows_spec.rb
@@ -1,38 +1,34 @@
 require "rails_helper"
 
 feature "visitors can view shows on index page" do
-  before(:each) do
-    @shows_list = []
-    2.times do
-      @shows_list << FactoryGirl.create(:show)
-    end
-  end
+  let!(:first_show) { FactoryGirl.create(:show) }
+  let!(:second_show) { FactoryGirl.create(:show) }
 
   scenario "visitor sees all shows displayed" do
     visit root_path
 
     expect(page).to have_content "Must Watch TV Shows"
-    expect(page).to have_content @shows_list[0].description
-    expect(page).to have_content @shows_list[1].description
-    expect(page).to have_link @shows_list[0].title
-    expect(page).to have_link @shows_list[1].title
+    expect(page).to have_content first_show.description
+    expect(page).to have_content second_show.description
+    expect(page).to have_link first_show.title
+    expect(page).to have_link second_show.title
   end
 
   scenario "visitor clicks on show title" do
     visit root_path
 
-    click_link @shows_list[0].title
+    click_link first_show.title
 
-    expect(current_path).to eq show_path(@shows_list[0])
+    expect(current_path).to eq show_path(first_show)
   end
 
   scenario "visitor sees shows info displayed" do
-    visit show_path(@shows_list[0])
+    visit show_path(first_show)
 
-    expect(page).to have_content @shows_list[0].title
-    expect(page).to have_content @shows_list[0].description
-    expect(page).to have_content @shows_list[0].average_rating
+    expect(page).to have_content first_show.title
+    expect(page).to have_content first_show.description
+    expect(page).to have_content first_show.average_rating
 
-    expect(page).to_not have_content @shows_list[1].title
+    expect(page).to_not have_content second_show.title
   end
 end
