951420d32b0bb5fc881e07c139d7ac1c3cfe0bc2
Harrison Muskat <harrisonmuskat@gmail.com>
2017-04-21 16:59:08 -0400

    Update syntax

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,7 @@ CarrierWave.configure do |config|
 
   if !Rails.env.test?
     config.fog_credentials = {
-      provider:              :'AWS',
+      provider:              :AWS,
       aws_access_key_id:     :ENV['AWS_ACCESS_KEY_ID'],
       aws_secret_access_key: :ENV['AWS_SECRET_ACCESS_KEY']
     }
