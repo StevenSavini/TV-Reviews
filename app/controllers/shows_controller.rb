acb909fb6dab13d4fbd426931d7618e67eb35659
amoralescr85 <amoralescr85@gmail.com>
2017-04-27 12:29:24 -0400

    hound

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -7,11 +7,11 @@ class ShowsController < ApplicationController
 
   def index
     @shows = Show.all
-    @shows = 
-    if params[:search]
-      Show.search(params[:search]).order("created_at DESC")
-    else
-      Show.all.order("created_at DESC")
-    end
+    @shows =
+      if params[:search]
+        Show.search(params[:search]).order("created_at DESC")
+      else
+        Show.all.order("created_at DESC")
+      end
   end
 end
